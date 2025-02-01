40be1f99d922f842f96fccca8178c21bb7909d3d
jlrindal <jeremyrindal06@gmail.com>
2025-02-01 17:01:26 -0500

    Update App.js

--- housing_dashboard/frontend/src/App.js
+++ housing_dashboard/frontend/src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import MobileHousingDashboard from './components/MobileHousingDashboard';
-
 function App() {
-  return (
-    <>
-      <Helmet>
-        <title>Housing Affordability Dashboard - Can You Afford It?</title>
-        <meta name="description" content="Explore housing affordability across US metro areas. Compare housing costs, income ratios, and affordability scores to understand market trends." />
-        <link rel="icon" href="/favicon.png" />
-      </Helmet>
-      <MobileHousingDashboard />
-    </>
-  );
+  return <MobileHousingDashboard />;
 }
-
 export default App;
